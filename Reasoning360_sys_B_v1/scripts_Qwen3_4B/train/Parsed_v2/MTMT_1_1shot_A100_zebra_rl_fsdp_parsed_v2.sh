defb724c1c6eb5c67a27a872831e4fd00cd446a5
asif6827 <asif6827@gmail.com>
2026-02-22 19:53:27 +0500

    update

--- Reasoning360_sys_B_v1/scripts_Qwen3_4B/train/Parsed_v2/MTMT_1_1shot_A100_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B_v1/scripts_Qwen3_4B/train/Parsed_v2/MTMT_1_1shot_A100_zebra_rl_fsdp_parsed_v2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -l
 
-#SBATCH -J MTMT-B-v1-H200 #job name
-#SBATCH -p gpu-H200 # queue used
+#SBATCH -J MTMT-B-v1-A100 #job name
+#SBATCH -p gpu-A100 # queue used
 #SBATCH --gres gpu:4 #number of gpus needed, default is 1
 #SBATCH -c 128  #number of CPUs needed, default is 1
 #SBATCH --mem 256GB #amount of memory needed, default
 #SBATCH --output=./all_logs/%j-%x.out
 #SBATCH --error=./all_logs/%j-%x.err
-#SBATCH -A H200
-#SBATCH -q h200_qos
+#SBATCH -A A100
+#SBATCH -q a100_qos
 #SBATCH --mail-user=asif6827@gmail.com
 
 
