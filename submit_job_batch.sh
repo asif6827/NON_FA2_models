df01302634680df64a29f2df99963f6a975ec48d
asif6827 <asif6827@gmail.com>
2026-02-22 05:51:43 +0500

    Update submit_job_batch.sh

--- submit_job_batch.sh
+++ submit_job_batch.sh
@@ -3,6 +3,17 @@ set -euo pipefail
 
 BASE_DIR="$HOME/NON_FA2_models"
 
+
+echo "Submitting Reasoning360_sys_B1..."
+cd "$BASE_DIR/Reasoning360_sys_B1"
+bash submit_job.sh
+
+
+echo "Submitting Reasoning360_sys_A..."
+cd "$BASE_DIR/Reasoning360_sys_A"
+bash submit_job.sh
+
+
 echo "Submitting Reasoning360_sys_B_v1..."
 cd "$BASE_DIR/Reasoning360_sys_B_v1"
 bash submit_job.sh
@@ -28,9 +39,6 @@ cd "$BASE_DIR/Reasoning360_sys_B_v5"
 bash submit_job.sh
 
 
-echo "Submitting Reasoning360_sys_A..."
-cd "$BASE_DIR/Reasoning360_sys_A"
-bash submit_job.sh
 
 
 
