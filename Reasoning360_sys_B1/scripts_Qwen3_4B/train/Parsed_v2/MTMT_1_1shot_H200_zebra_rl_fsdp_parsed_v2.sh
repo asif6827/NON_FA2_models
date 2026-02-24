ae1d08e8dc26420e91be611dba2021fe6febbaf3
asif6827 <asif6827@gmail.com>
2026-02-24 05:59:45 +0500

    update

--- Reasoning360_sys_B1/scripts_Qwen3_4B/train/Parsed_v2/MTMT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B1/scripts_Qwen3_4B/train/Parsed_v2/MTMT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
@@ -196,7 +196,7 @@ adv_estimator=grpo
 use_kl_in_reward=False
 kl_coef=0.0
 use_kl_loss=True
-kl_loss_coef=0.01
+kl_loss_coef=0.1
 
 
 clip_ratio_low=0.2
@@ -221,7 +221,7 @@ max_num_gen_batches=10
 #train_prompt_mini_bsz=4  # model grad update batchsize
 
 
-train_prompt_bsz=168  # on-policy model update batchsize: train_prompt_bsz * rollout.n
+train_prompt_bsz=56  # on-policy model update batchsize: train_prompt_bsz * rollout.n
 gen_prompt_bsz=$((train_prompt_bsz * 1))
 n_resp_per_prompt=8
 train_prompt_mini_bsz=14  # model grad update batchsize
@@ -312,6 +312,7 @@ python -m recipe.dapo.main_dapo \
     actor_rollout_ref.rollout.val_kwargs.temperature=${TEST_TEMP} \
     actor_rollout_ref.rollout.val_kwargs.n=1 \
     actor_rollout_ref.rollout.val_kwargs.do_sample=True \
+    actor_rollout_ref.rollout.dtype=bfloat16 \
     actor_rollout_ref.model.path=$BASE_MODEL \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.rollout.multi_turn.enable=False \
