aeae8ed9bb2b3f3902b1eee6f44fd3d54b1fa12e
asif6827 <asif6827@gmail.com>
2026-02-26 04:57:28 +0500

    update

--- Reasoning360_sys_B1/scripts_Qwen3_4B/train/Parsed_v2/MTMT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B1/scripts_Qwen3_4B/train/Parsed_v2/MTMT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
@@ -231,10 +231,10 @@ max_num_gen_batches=10
 #train_prompt_mini_bsz=4  # model grad update batchsize
 
 
-train_prompt_bsz=56  # on-policy model update batchsize: train_prompt_bsz * rollout.n
+train_prompt_bsz=128  # on-policy model update batchsize: train_prompt_bsz * rollout.n
 gen_prompt_bsz=$((train_prompt_bsz * 1))
 n_resp_per_prompt=8
-train_prompt_mini_bsz=14  # model grad update batchsize
+train_prompt_mini_bsz=16  # model grad update batchsize
 
 
 
