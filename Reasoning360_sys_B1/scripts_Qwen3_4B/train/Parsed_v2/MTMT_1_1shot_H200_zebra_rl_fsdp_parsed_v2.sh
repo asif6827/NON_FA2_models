34e529490213ae69e68f16d8f57b21f6b66a7962
asif6827 <asif6827@gmail.com>
2026-02-26 12:34:20 +0500

    Update MTMT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh

--- Reasoning360_sys_B1/scripts_Qwen3_4B/train/Parsed_v2/MTMT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B1/scripts_Qwen3_4B/train/Parsed_v2/MTMT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
@@ -2,7 +2,6 @@
 
 #SBATCH -J MTMT-B1-H200 #job name
 #SBATCH -p gpu-H200 # queue used
-#SBATCH --exclude=crirdchpxd005
 #SBATCH --gres gpu:4 #number of gpus needed, default is 1
 #SBATCH -c 128  #number of CPUs needed, default is 1
 #SBATCH --mem 256GB #amount of memory needed, default
