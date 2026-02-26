6b25cd408924ba252b6af665b05e93a964a3d73a
asif6827 <asif6827@gmail.com>
2026-02-26 22:46:33 +0500

    update to 50 epochs

--- submit_job_batch.sh
+++ submit_job_batch.sh
@@ -9,11 +9,6 @@ cd "$BASE_DIR/Reasoning360_sys_B1"
 bash submit_job.sh
 
 
-echo "Submitting Reasoning360_sys_B1..."
-cd "$BASE_DIR/Reasoning360_sys_B1"
-bash submit_job.sh
-
-
 
 #if false; then
 echo "Submitting Reasoning360_sys_A..."
