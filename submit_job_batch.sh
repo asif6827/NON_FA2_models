f70206795297ed3ef2d989e305a6cc7c3579f828
asif6827 <asif6827@gmail.com>
2026-03-02 17:11:15 +0500

    Update submit_job_batch.sh

--- submit_job_batch.sh
+++ submit_job_batch.sh
@@ -48,17 +48,6 @@ fi
 
 #if false; then
 
-
-echo "Submitting Reasoning360_sys_B1..."
-cd "$BASE_DIR/Reasoning360_sys_B1"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_B1..."
-cd "$BASE_DIR/Reasoning360_sys_B1"
-bash submit_job.sh
-
-
 echo "Submitting Reasoning360_sys_A..."
 cd "$BASE_DIR/Reasoning360_sys_A"
 bash submit_job.sh
@@ -68,49 +57,13 @@ echo "Submitting Reasoning360_sys_A..."
 cd "$BASE_DIR/Reasoning360_sys_A"
 bash submit_job.sh
 
-
-echo "Submitting Reasoning360_sys_B_v1..."
-cd "$BASE_DIR/Reasoning360_sys_B_v1"
+echo "Submitting Reasoning360_sys_B1..."
+cd "$BASE_DIR/Reasoning360_sys_B1"
 bash submit_job.sh
 
 
-echo "Submitting Reasoning360_sys_B_v5..."
-cd "$BASE_DIR/Reasoning360_sys_B_v5"
-bash submit_job.sh
-
-echo "Submitting Reasoning360_sys_B_v6..."
-cd "$BASE_DIR/Reasoning360_sys_B_v6"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_B_v7..."
-cd "$BASE_DIR/Reasoning360_sys_B_v7"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_B_v8..."
-cd "$BASE_DIR/Reasoning360_sys_B_v8"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_B_v9..."
-cd "$BASE_DIR/Reasoning360_sys_B_v9"
-bash submit_job.sh
-
-
-
-echo "Submitting Reasoning360_sys_B_v2..."
-cd "$BASE_DIR/Reasoning360_sys_B_v2"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_B_v3..."
-cd "$BASE_DIR/Reasoning360_sys_B_v3"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_B_v4..."
-cd "$BASE_DIR/Reasoning360_sys_B_v4"
+echo "Submitting Reasoning360_sys_B1..."
+cd "$BASE_DIR/Reasoning360_sys_B1"
 bash submit_job.sh
 
 
