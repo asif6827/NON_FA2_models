4e1558307e47b8fe370a0aba8c51d7635bd221d1
asif6827 <asif6827@gmail.com>
2026-03-03 05:53:01 +0500

    update

--- submit_job_batch.sh
+++ submit_job_batch.sh
@@ -42,32 +42,26 @@ if false; then
     bash submit_job.sh
 
 
+    echo "Submitting Reasoning360_sys_A..."
+    cd "$BASE_DIR/Reasoning360_sys_A"
+    bash submit_job.sh
+
+
+    echo "Submitting Reasoning360_sys_A..."
+    cd "$BASE_DIR/Reasoning360_sys_A"
+    bash submit_job.sh
+
+
+    echo "Submitting Reasoning360_sys_B1..."
+    cd "$BASE_DIR/Reasoning360_sys_B1"
+    bash submit_job.sh
+
+
+    echo "Submitting Reasoning360_sys_B1..."
+    cd "$BASE_DIR/Reasoning360_sys_B1"
+    bash submit_job.sh
+
+
 fi
-
-
-
-#if false; then
-
-echo "Submitting Reasoning360_sys_A..."
-cd "$BASE_DIR/Reasoning360_sys_A"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_A..."
-cd "$BASE_DIR/Reasoning360_sys_A"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_B1..."
-cd "$BASE_DIR/Reasoning360_sys_B1"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_B1..."
-cd "$BASE_DIR/Reasoning360_sys_B1"
-bash submit_job.sh
-
-
-#fi
 
 echo "All submission commands executed."
