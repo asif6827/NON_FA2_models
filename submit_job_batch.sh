d3c1a0c985f82475821420dae94368cce12e65a0
asif6827 <asif6827@gmail.com>
2026-03-04 13:32:06 +0500

    update

--- submit_job_batch.sh
+++ submit_job_batch.sh
@@ -4,41 +4,45 @@ set -euo pipefail
 BASE_DIR="$HOME/NON_FA2_models"
 
 
-echo "Submitting Reasoning360_sys_A..."
-cd "$BASE_DIR/Reasoning360_sys_A"
+
+echo "Submitting Reasoning360_sys_B_v11..."
+cd "$BASE_DIR/Reasoning360_sys_B_v11"
 bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_A..."
-cd "$BASE_DIR/Reasoning360_sys_A"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_A..."
-cd "$BASE_DIR/Reasoning360_sys_A"
-bash submit_job.sh
-
-
-
-echo "Submitting Reasoning360_sys_B1..."
-cd "$BASE_DIR/Reasoning360_sys_B1"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_B1..."
-cd "$BASE_DIR/Reasoning360_sys_B1"
-bash submit_job.sh
-
-
-echo "Submitting Reasoning360_sys_B1..."
-cd "$BASE_DIR/Reasoning360_sys_B1"
-bash submit_job.sh
-
-
 
 
 
 if false; then
+
+    echo "Submitting Reasoning360_sys_A..."
+    cd "$BASE_DIR/Reasoning360_sys_A"
+    bash submit_job.sh
+
+
+    echo "Submitting Reasoning360_sys_A..."
+    cd "$BASE_DIR/Reasoning360_sys_A"
+    bash submit_job.sh
+
+
+    echo "Submitting Reasoning360_sys_A..."
+    cd "$BASE_DIR/Reasoning360_sys_A"
+    bash submit_job.sh
+
+
+
+    echo "Submitting Reasoning360_sys_B1..."
+    cd "$BASE_DIR/Reasoning360_sys_B1"
+    bash submit_job.sh
+
+
+    echo "Submitting Reasoning360_sys_B1..."
+    cd "$BASE_DIR/Reasoning360_sys_B1"
+    bash submit_job.sh
+
+
+    echo "Submitting Reasoning360_sys_B1..."
+    cd "$BASE_DIR/Reasoning360_sys_B1"
+    bash submit_job.sh
+
 
     echo "Submitting Reasoning360_sys_A..."
     cd "$BASE_DIR/Reasoning360_sys_A"
