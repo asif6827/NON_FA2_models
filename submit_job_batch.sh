de3b9a26429824614d23d1ad5c1566dfdcf5920f
asif6827 <asif6827@gmail.com>
2026-03-10 13:15:46 +0500

    Update submit_job_batch.sh

--- submit_job_batch.sh
+++ submit_job_batch.sh
@@ -6,8 +6,8 @@ BASE_DIR="$HOME/NON_FA2_models"
 
 
 
-echo "Submitting Reasoning360_sys_C_v1..."
-cd "$BASE_DIR/Reasoning360_sys_C_v1"
+echo "Submitting Reasoning360_sys_C_v2..."
+cd "$BASE_DIR/Reasoning360_sys_C_v2"
 bash submit_job.sh
 
 
