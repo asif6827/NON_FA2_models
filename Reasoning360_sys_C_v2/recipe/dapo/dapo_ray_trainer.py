6a4c1d9a60da4c55584567b7f50742aa1e6d49e4
asif6827 <asif6827@gmail.com>
2026-03-11 15:58:16 +0500

    Update dapo_ray_trainer.py

--- Reasoning360_sys_C_v2/recipe/dapo/dapo_ray_trainer.py
+++ Reasoning360_sys_C_v2/recipe/dapo/dapo_ray_trainer.py
@@ -476,16 +476,6 @@ class RayDAPOTrainer(RayPPOTrainer):
                                     self._save_checkpoint()
                             self.puzzle_acc = puzzle_accuracy
 
-                    with marked_timer("stop_profile", timing_raw):
-                        if do_profile:
-                            self.actor_rollout_wg.stop_profile()
-                            if self.use_reference_policy:
-                                self.ref_policy_wg.stop_profile()
-                            if self.use_critic:
-                                self.critic_wg.stop_profile()
-                            if self.use_rm:
-                                self.rm_wg.stop_profile()
-
                     print(json.dumps(metrics, indent=2, sort_keys=True))
 
                     progress_bar.update(1)
