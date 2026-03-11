3734cca48eb4c012fe8bf941c6111b0667a97acb
asif6827 <asif6827@gmail.com>
2026-03-11 19:36:20 +0500

    Update submit_job_batch_qwen3_4B.sh

--- submit_job_batch_qwen3_4B.sh
+++ submit_job_batch_qwen3_4B.sh
@@ -6,9 +6,9 @@ BASE_DIR="$HOME/NON_FA2_models"
 
 
 
-echo "Submitting Reasoning360_sys_C_v2..."
-cd "$BASE_DIR/Reasoning360_sys_C_v2"
-bash submit_job.sh
+echo "Submitting Reasoning360_sys_B_v22..."
+cd "$BASE_DIR/Reasoning360_sys_B_v22"
+bash submit_job_qwen3.sh
 
 
 
