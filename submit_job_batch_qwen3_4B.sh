9a5faf0c16689aab2e02f52b1748d70cb44872d4
asif6827 <asif6827@gmail.com>
2026-03-12 21:12:59 +0500

    update

--- submit_job_batch_qwen3_4B.sh
+++ submit_job_batch_qwen3_4B.sh
@@ -4,15 +4,16 @@ set -euo pipefail
 BASE_DIR="$HOME/NON_FA2_models"
 
 
+echo "Submitting Reasoning360_sys_B_v22..."
+cd "$BASE_DIR/Reasoning360_sys_B_v22"
+bash submit_job_qwen3.sh
+
 
 echo "Submitting Reasoning360_sys_C_v2..."
 cd "$BASE_DIR/Reasoning360_sys_C_v2"
 bash submit_job_qwen3.sh
 
 
-echo "Submitting Reasoning360_sys_B_v22..."
-cd "$BASE_DIR/Reasoning360_sys_B_v22"
-bash submit_job_qwen3.sh
 
 
 
