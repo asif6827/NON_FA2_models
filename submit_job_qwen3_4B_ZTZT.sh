4a61762b2dd9ad61c451c1767241acd67796549f
asif6827 <asif6827@gmail.com>
2026-03-15 00:19:15 +0500

    update

--- submit_job_qwen3_4B_ZTZT.sh
+++ submit_job_qwen3_4B_ZTZT.sh
@@ -5,45 +5,41 @@ set -euo pipefail
 BASE_DIR="$HOME/NON_FA2_models"
 
 
-echo "Submitting Reasoning360_sys_C_v2..."
-cd "$BASE_DIR/Reasoning360_sys_C_v2"
-bash submit_job_qwen3.sh
+echo "Submitting Reasoning360_sys_A..."
+cd "$BASE_DIR/Reasoning360_sys_A"
+bash submit_job_qwen3_ZTZT.sh
 
 
-echo "Submitting Reasoning360_sys_C_v3..."
-cd "$BASE_DIR/Reasoning360_sys_C_v3"
-bash submit_job_qwen3.sh
+echo "Submitting Reasoning360_sys_B1..."
+cd "$BASE_DIR/Reasoning360_sys_B1"
+bash submit_job_qwen3_ZTZT.sh
 
 
-echo "Submitting Reasoning360_sys_C_v4..."
-cd "$BASE_DIR/Reasoning360_sys_C_v4"
-bash submit_job_qwen3.sh
+echo "Submitting Reasoning360_sys_B_v1..."
+cd "$BASE_DIR/Reasoning360_sys_B_v1"
+bash submit_job_qwen3_ZTZT.sh
 
-
-echo "Submitting Reasoning360_sys_C_v5..."
-cd "$BASE_DIR/Reasoning360_sys_C_v5"
-bash submit_job_qwen3.sh
 
 
 echo "Submitting Reasoning360_sys_C_v2..."
 cd "$BASE_DIR/Reasoning360_sys_C_v2"
-bash submit_job_qwen3.sh
+bash submit_job_qwen3_ZTZT.sh
 
 
 echo "Submitting Reasoning360_sys_C_v3..."
 cd "$BASE_DIR/Reasoning360_sys_C_v3"
-bash submit_job_qwen3.sh
+bash submit_job_qwen3_ZTZT.sh
 
 
 
 echo "Submitting Reasoning360_sys_C_v4..."
 cd "$BASE_DIR/Reasoning360_sys_C_v4"
-bash submit_job_qwen3.sh
+bash submit_job_qwen3_ZTZT.sh
 
 
 echo "Submitting Reasoning360_sys_C_v5..."
 cd "$BASE_DIR/Reasoning360_sys_C_v5"
-bash submit_job_qwen3.sh
+bash submit_job_qwen3_ZTZT.sh
 
 
 
