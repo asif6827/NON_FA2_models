2bf87726ca2fb9a3513b27bc40a19d283a35334e
asif6827 <asif6827@gmail.com>
2026-03-14 22:13:16 +0500

    Update

--- Reasoning360_sys_B_v5/scripts_Qwen3_4B/train/Parsed_v2/ZTZT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B_v5/scripts_Qwen3_4B/train/Parsed_v2/ZTZT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -l
 
-#SBATCH -J MTMT-B-v5-H200 #job name
+#SBATCH -J ZTZT-B-v5-H200 #job name
 #SBATCH -p gpu-H200 # queue used
 #SBATCH --nodelist=crirdchpxd003
 #SBATCH --gres gpu:4 #number of gpus needed, default is 1
@@ -89,7 +89,7 @@ NUM_GPUS=4 # Set the number of GPUs to use on this node
 gpu_memory_utilization=0.8
 # --- Resuming & Logging ---
 RESUME_CKPT_DIR_NAME=""  # Fill in the W&B experiment name to resume from, otherwise leave empty to start from scratch
-WANDB_PROJECT="Sys_B_v5_Qwen3_4B_MTMT_1_1shot_H200" # Your wandb project name
+WANDB_PROJECT="Sys_B_v5_Qwen3_4B_ZTZT_1_1shot_H200" # Your wandb project name
 
 # --- External Services ---
 export STEM_LLM_JUDGE_URL="<STEM_LLM_JUDGE_URL>"  # Optional: Fill in the llm-as-judge hosted URL for 'STEM' domain evaluation
