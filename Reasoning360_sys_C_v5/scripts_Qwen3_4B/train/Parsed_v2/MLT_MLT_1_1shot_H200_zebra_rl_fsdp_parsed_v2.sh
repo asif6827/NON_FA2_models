54f4c1d52bded109d4b76dc69065f8de182623bc
asif6827 <asif6827@gmail.com>
2026-03-15 16:54:40 +0500

    update response length

--- Reasoning360_sys_C_v5/scripts_Qwen3_4B/train/Parsed_v2/MLT_MLT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_C_v5/scripts_Qwen3_4B/train/Parsed_v2/MLT_MLT_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
@@ -218,7 +218,7 @@ clip_ratio_low=0.2
 clip_ratio_high=0.2
 
 max_prompt_length=$((1024 * 8))
-max_response_length=$((1024 * 8))
+max_response_length=$((1024 * 4))
 enable_overlong_buffer=False
 overlong_buffer_len=$((1024 * 4))
 overlong_penalty_factor=1.0
