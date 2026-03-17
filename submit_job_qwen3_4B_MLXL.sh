8325b597d0eeede64bda0a3199e44ccb54c6f615
asif6827 <asif6827@gmail.com>
2026-03-17 23:38:50 +0500

    Update submit_job_qwen3_4B_MLXL.sh

--- submit_job_qwen3_4B_MLXL.sh
+++ submit_job_qwen3_4B_MLXL.sh
@@ -9,68 +9,129 @@ if false; then
     echo "Submitting Reasoning360_sys_A..."
     cd "$BASE_DIR/Reasoning360_sys_A"
     bash submit_job_qwen3_ZTZT_A100.sh
+
+
+
+    echo "Submitting Reasoning360_sys_A..."
+    cd "$BASE_DIR/Reasoning360_sys_A"
+    bash submit_job_qwen3_MLXL.sh
+
+    echo "Submitting Reasoning360_sys_B1..."
+    cd "$BASE_DIR/Reasoning360_sys_B1"
+    bash submit_job_qwen3_MLXL.sh
+
+
+    echo "Submitting Reasoning360_sys_B_v1..."
+    cd "$BASE_DIR/Reasoning360_sys_B_v1"
+    bash submit_job_qwen3_MLXL.sh
+
+
+    echo "Submitting Reasoning360_sys_B_v4..."
+    cd "$BASE_DIR/Reasoning360_sys_B_v4"
+    bash submit_job_qwen3_MLXL.sh
+
+
+    echo "Submitting Reasoning360_sys_B_v7..."
+    cd "$BASE_DIR/Reasoning360_sys_B_v7"
+    bash submit_job_qwen3_MLXL.sh
+
+
+    echo "Submitting Reasoning360_sys_B_v11..."
+    cd "$BASE_DIR/Reasoning360_sys_B_v11"
+    bash submit_job_qwen3_MLXL.sh
+
+
+    echo "Submitting Reasoning360_sys_B_v2..."
+    cd "$BASE_DIR/Reasoning360_sys_B_v2"
+    bash submit_job_qwen3_MLXL.sh
+
+
+    echo "Submitting Reasoning360_sys_B_v3..."
+    cd "$BASE_DIR/Reasoning360_sys_B_v3"
+    bash submit_job_qwen3_MLXL.sh
+
+    echo "Submitting Reasoning360_sys_B_v5..."
+    cd "$BASE_DIR/Reasoning360_sys_B_v5"
+    bash submit_job_qwen3_MLXL.sh
+
+
+    echo "Submitting Reasoning360_sys_B_v6..."
+    cd "$BASE_DIR/Reasoning360_sys_B_v6"
+    bash submit_job_qwen3_MLXL.sh
+
+
+    echo "Submitting Reasoning360_sys_B_v8..."
+    cd "$BASE_DIR/Reasoning360_sys_B_v8"
+    bash submit_job_qwen3_MLXL.sh
+
+
+    echo "Submitting Reasoning360_sys_B_v9..."
+    cd "$BASE_DIR/Reasoning360_sys_B_v9"
+    bash submit_job_qwen3_MLXL.sh
 fi
 
 
-
-
-
-echo "Submitting Reasoning360_sys_A..."
-cd "$BASE_DIR/Reasoning360_sys_A"
-bash submit_job_qwen3_MLXL.sh
-
-echo "Submitting Reasoning360_sys_B1..."
-cd "$BASE_DIR/Reasoning360_sys_B1"
+echo "Submitting Reasoning360_sys_B_v10..."
+cd "$BASE_DIR/Reasoning360_sys_B_v10"
 bash submit_job_qwen3_MLXL.sh
 
 
-echo "Submitting Reasoning360_sys_B_v1..."
-cd "$BASE_DIR/Reasoning360_sys_B_v1"
+echo "Submitting Reasoning360_sys_B_v12..."
+cd "$BASE_DIR/Reasoning360_sys_B_v12"
 bash submit_job_qwen3_MLXL.sh
 
 
-echo "Submitting Reasoning360_sys_B_v4..."
-cd "$BASE_DIR/Reasoning360_sys_B_v4"
+echo "Submitting Reasoning360_sys_B_v13..."
+cd "$BASE_DIR/Reasoning360_sys_B_v13"
 bash submit_job_qwen3_MLXL.sh
 
 
-echo "Submitting Reasoning360_sys_B_v7..."
-cd "$BASE_DIR/Reasoning360_sys_B_v7"
+echo "Submitting Reasoning360_sys_B_v14..."
+cd "$BASE_DIR/Reasoning360_sys_B_v14"
 bash submit_job_qwen3_MLXL.sh
 
 
-echo "Submitting Reasoning360_sys_B_v11..."
-cd "$BASE_DIR/Reasoning360_sys_B_v11"
+echo "Submitting Reasoning360_sys_B_v15..."
+cd "$BASE_DIR/Reasoning360_sys_B_v15"
 bash submit_job_qwen3_MLXL.sh
 
 
-echo "Submitting Reasoning360_sys_B_v2..."
-cd "$BASE_DIR/Reasoning360_sys_B_v2"
+echo "Submitting Reasoning360_sys_B_v16..."
+cd "$BASE_DIR/Reasoning360_sys_B_v16"
 bash submit_job_qwen3_MLXL.sh
 
 
-echo "Submitting Reasoning360_sys_B_v3..."
-cd "$BASE_DIR/Reasoning360_sys_B_v3"
-bash submit_job_qwen3_MLXL.sh
-
-echo "Submitting Reasoning360_sys_B_v5..."
-cd "$BASE_DIR/Reasoning360_sys_B_v5"
+echo "Submitting Reasoning360_sys_B_v17..."
+cd "$BASE_DIR/Reasoning360_sys_B_v17"
 bash submit_job_qwen3_MLXL.sh
 
 
-echo "Submitting Reasoning360_sys_B_v6..."
-cd "$BASE_DIR/Reasoning360_sys_B_v6"
+echo "Submitting Reasoning360_sys_B_v18..."
+cd "$BASE_DIR/Reasoning360_sys_B_v18"
 bash submit_job_qwen3_MLXL.sh
 
 
-echo "Submitting Reasoning360_sys_B_v8..."
-cd "$BASE_DIR/Reasoning360_sys_B_v8"
+echo "Submitting Reasoning360_sys_B_v19..."
+cd "$BASE_DIR/Reasoning360_sys_B_v19"
 bash submit_job_qwen3_MLXL.sh
 
 
-echo "Submitting Reasoning360_sys_B_v9..."
-cd "$BASE_DIR/Reasoning360_sys_B_v9"
+echo "Submitting Reasoning360_sys_B_v20..."
+cd "$BASE_DIR/Reasoning360_sys_B_v20"
 bash submit_job_qwen3_MLXL.sh
+
+
+echo "Submitting Reasoning360_sys_B_v21..."
+cd "$BASE_DIR/Reasoning360_sys_B_v21"
+bash submit_job_qwen3_MLXL.sh
+
+
+echo "Submitting Reasoning360_sys_B_v22..."
+cd "$BASE_DIR/Reasoning360_sys_B_v22"
+bash submit_job_qwen3_MLXL.sh
+
+
+
 
 echo "Submitting Reasoning360_sys_C_v2..."
 cd "$BASE_DIR/Reasoning360_sys_C_v2"
