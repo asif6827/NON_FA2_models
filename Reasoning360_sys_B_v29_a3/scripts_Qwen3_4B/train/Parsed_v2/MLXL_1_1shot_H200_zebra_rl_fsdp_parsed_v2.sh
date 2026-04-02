f376fd891c1b3e724529dfd5f37068d4e0b9a58a
asif6827 <asif6827@gmail.com>
2026-04-02 18:20:35 +0500

    update

--- Reasoning360_sys_B_v29_a3/scripts_Qwen3_4B/train/Parsed_v2/MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B_v29_a3/scripts_Qwen3_4B/train/Parsed_v2/MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -J MLXL-B-v29-a3-H200 #job name
 #SBATCH -p gpu-H200 # queue used
-#SBATCH --exclude=crirdchpxd001,crirdchpxd003,crirdchpxd005
+#SBATCH --nodelist=crirdchpxd002
 #SBATCH --gres gpu:4 #number of gpus needed, default is 1
 #SBATCH -c 128  #number of CPUs needed, default is 1
 #SBATCH --mem 256GB #amount of memory needed, default
--- Reasoning360_sys_B_v29_a3/verl/utils/reward_score/our_puzzle_dataset_v6.py
+++ Reasoning360_sys_B_v29_a3/verl/utils/reward_score/our_puzzle_dataset_v6.py
@@ -1020,7 +1020,7 @@ def compute_score(
                 )
 
                 process_bonus = (
-                        0.40 * novel_step_score
+                        0.70 * novel_step_score
                         + 0.30 * consistency_score
                         - 0.15 * contradiction_ratio
                 )
