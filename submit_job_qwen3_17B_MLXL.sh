99a1a66539bde689cc532b698842ad82c591e780
asif6827 <asif6827@gmail.com>
2026-04-05 23:03:45 +0500

    update for v9 for A100

--- submit_job_qwen3_17B_MLXL.sh
+++ submit_job_qwen3_17B_MLXL.sh
@@ -33,44 +33,44 @@ if false; then
 
     echo "Submitting Reasoning360_sys_B1..."
     cd "$BASE_DIR/Reasoning360_sys_B1"
-    bash submit_job_qwen25_15B_MLXL.sh
+    bash submit_job_qwen3_17B_MLXL.sh
 
     echo "Submitting Reasoning360_sys_B1..."
     cd "$BASE_DIR/Reasoning360_sys_B1"
-    bash submit_job_qwen25_15B_MLXL.sh
+    bash submit_job_qwen3_17B_MLXL.sh
 
     echo "Submitting Reasoning360_sys_B1..."
     cd "$BASE_DIR/Reasoning360_sys_B1"
-    bash submit_job_qwen25_15B_MLXL.sh
+    bash submit_job_qwen3_17B_MLXL.sh
 
     echo "Submitting Reasoning360_sys_B1..."
     cd "$BASE_DIR/Reasoning360_sys_B1"
-    bash submit_job_qwen25_15B_MLXL.sh
+    bash submit_job_qwen3_17B_MLXL.sh
 
 
 
 
     echo "Submitting Reasoning360_sys_B_v29..."
     cd "$BASE_DIR/Reasoning360_sys_B_v29"
-    bash submit_job_qwen25_15B_MLXL.sh
+    bash submit_job_qwen3_17B_MLXL.sh
 
     echo "Submitting Reasoning360_sys_B_v29..."
     cd "$BASE_DIR/Reasoning360_sys_B_v29"
-    bash submit_job_qwen25_15B_MLXL.sh
+    bash submit_job_qwen3_17B_MLXL.sh
 
     echo "Submitting Reasoning360_sys_B_v29..."
     cd "$BASE_DIR/Reasoning360_sys_B_v29"
-    bash submit_job_qwen25_15B_MLXL.sh
+    bash submit_job_qwen3_17B_MLXL.sh
 
     echo "Submitting Reasoning360_sys_B_v29..."
     cd "$BASE_DIR/Reasoning360_sys_B_v29"
-    bash submit_job_qwen25_15B_MLXL.sh
+    bash submit_job_qwen3_17B_MLXL.sh
 
 
 
     echo "Submitting Reasoning360_sys_B_v29..."
     cd "$BASE_DIR/Reasoning360_sys_B_v29"
-    bash submit_job_qwen25_15B_MLXL.sh
+    bash submit_job_qwen3_17B_MLXL.sh
 fi
 
 
