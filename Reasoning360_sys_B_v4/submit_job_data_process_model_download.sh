2d8808fdf1c3defad65701c4ed7fe5d90b7fdb17
asif6827 <asif6827@gmail.com>
2026-04-06 23:42:56 +0500

    Create submit_job_data_process_model_download.sh

---
+++ Reasoning360_sys_B_v4/submit_job_data_process_model_download.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#echo " I am only this..!"
+
+
+#echo "Data Pre-Processing Runing"
+#sbatch ./data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
+
+
+echo "Model Downloading...!"
+sbatch ./scripts_data_etc/download_hub_llama.sh
+
+
+#cho "Data Pre-Processing Runing"
+#sbatch ./data_preprocess/logic/data_preprocess_parsed_v7.sh
+
