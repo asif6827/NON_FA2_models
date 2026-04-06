2cde3ddb884f4d7e788f6fdbda297ee2cef81184
asif6827 <asif6827@gmail.com>
2026-04-07 00:19:46 +0500

    Update download_hub_LLama3.py

--- Reasoning360_sys_B_v4/scripts_data_etc/download_hub_LLama3.py
+++ Reasoning360_sys_B_v4/scripts_data_etc/download_hub_LLama3.py
@@ -5,17 +5,15 @@ import os
 repo_id = "meta-llama/Llama-3.2-3B-Instruct"
 target = Path("/export/home/asifali/HF_cache/Llama-3.2-3B-Instruct")
 
-token = os.environ["HF_TOKEN"]
-
 path = snapshot_download(
     repo_id=repo_id,
-    repo_type="model",
     local_dir=target,
     local_dir_use_symlinks=False,
+    token=os.getenv("HF_TOKEN"),
     resume_download=True,
-    local_files_only=False,
-    token=token,
-)
+    local_files_only=False,)
+
+print(path)
 
 print("Downloaded to:", path)
 
