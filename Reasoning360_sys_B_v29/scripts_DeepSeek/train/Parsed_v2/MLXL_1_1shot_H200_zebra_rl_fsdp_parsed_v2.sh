f031b9a157f82a37e15c272fbe429ddc025419af
asif6827 <asif6827@gmail.com>
2026-04-19 20:12:55 +0500

    update

--- Reasoning360_sys_B_v29/scripts_DeepSeek/train/Parsed_v2/MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B_v29/scripts_DeepSeek/train/Parsed_v2/MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -J MLXL-B-v29-H200-DS-1-5B #job name
 #SBATCH -p gpu-H200 # queue used
-#SBATCH --nodelist=crirdchpxd002,crirdchpxd004
+#SBATCH --exclude=crirdchpxd001,crirdchpxd003,crirdchpxd005
 #SBATCH --gres gpu:4 #number of gpus needed, default is 1
 #SBATCH -c 128  #number of CPUs needed, default is 1
 #SBATCH --mem 256GB #amount of memory needed, default
@@ -165,10 +165,10 @@ TEST_DATA_DIR=${SHARED_DATA_PATH}/test
 
 
 ### Logic (train)
-zebra_train_path=${TRAIN_DATA_DIR}/logic_our_zebra_puzzle_new_reward_204.parquet
+zebra_train_path=${TRAIN_DATA_DIR}/logic_our_zebra_puzzle_new_reward_300.parquet
 
 ### Logic (test)
-zebralogic_test_path=${TEST_DATA_DIR}/logic_our_zebra_puzzle_new_reward_test_476.parquet
+zebralogic_test_path=${TEST_DATA_DIR}/logic_our_zebra_puzzle_new_reward_test_700.parquet
 
 
 train_files="['${zebra_train_path}']"  # Use math as example, add to more tasks as needed
@@ -230,10 +230,10 @@ max_num_gen_batches=10
 #train_prompt_mini_bsz=4  # model grad update batchsize
 
 
-train_prompt_bsz=68  # on-policy model update batchsize: train_prompt_bsz * rollout.n
+train_prompt_bsz=100  # on-policy model update batchsize: train_prompt_bsz * rollout.n
 gen_prompt_bsz=$((train_prompt_bsz * 1))
 n_resp_per_prompt=8
-train_prompt_mini_bsz=17  # model grad update batchsize
+train_prompt_mini_bsz=20  # model grad update batchsize
 
 
 
