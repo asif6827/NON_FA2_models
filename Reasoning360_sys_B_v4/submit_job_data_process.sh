066a3013cb226d89df0b9cdbfd3c7ccdf4af3d01
asif6827 <asif6827@gmail.com>
2026-04-19 19:30:26 +0500

    Update submit_job_data_process.sh

--- Reasoning360_sys_B_v4/submit_job_data_process.sh
+++ Reasoning360_sys_B_v4/submit_job_data_process.sh
@@ -6,3 +6,5 @@ echo "Data Processing script Running..!"
 sbatch ./data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
 
 
+#echo "Data Pre-Processing Runing"
+#sbatch ./data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
