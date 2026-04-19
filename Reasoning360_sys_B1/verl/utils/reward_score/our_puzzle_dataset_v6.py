37f48cc2094c782d3d49af2c16e912196b7eb45d
asif6827 <asif6827@gmail.com>
2026-04-19 23:08:02 +0500

    update rewards

--- Reasoning360_sys_B1/verl/utils/reward_score/our_puzzle_dataset_v6.py
+++ Reasoning360_sys_B1/verl/utils/reward_score/our_puzzle_dataset_v6.py
@@ -985,7 +985,7 @@ def compute_score(
     final_reward = 0.0
     try:
 
-        final_reward = 0.6 * float(puzzle_acc_score) + 0.2 * parsing_reward + 0.005 * float(cell_acc_score)
+        final_reward = 0.6 * float(puzzle_acc_score) + 0.2 * parsing_reward
         normalizer = 0.0
 
         # -----------------------
