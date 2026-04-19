c82be5ac6aa84063c217d33903b4fc38730a5461
asif6827 <asif6827@gmail.com>
2026-04-19 19:25:39 +0500

    Update data pre-processing

--- Reasoning360_sys_B_v4/data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
+++ Reasoning360_sys_B_v4/data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
@@ -21,19 +21,19 @@ source activate Reasoning360
 #python data_spliting_panther.py
 
 
-python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_STST.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'small_train_small_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_STST'
+#python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_STST.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'small_train_small_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_STST'
 
-python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_MTMT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'med_train_med_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_MTMT'
+#python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_MTMT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'med_train_med_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_MTMT'
 
-python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_LTLT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'large_train_large_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_LTLT'
+#python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_LTLT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'large_train_large_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_LTLT'
 
-python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_XTXT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'xl_train_xl_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_XTXT'
+#python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_XTXT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'xl_train_xl_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_XTXT'
 
-python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_MLT_MLT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'ml_train_ml_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_MLT_MLT'
+#python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_MLT_MLT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'ml_train_ml_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_MLT_MLT'
 
 python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_MLXL.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'mlxl_train_mlxl_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_MLXL'
 
-python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_ZTZT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'zl_train_zl_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_ZTZT'
+#python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_ZTZT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'zl_train_zl_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_ZTZT'
 
 #python ./data_preprocess/logic/our_pre_process_guru_readjust_parsed_v2.py --input_path '/export/home/asifali/HF_cache/guru_data' --output_dir '/export/home/asifali/HF_cache/guru_data_adjusted_parsed_v2'
 
---
+++ Reasoning360_sys_B_v4/submit_job_data_process.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#echo " I am only this..!"
+
+echo "Data Processing script Running..!"
+sbatch ./data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
+
+
