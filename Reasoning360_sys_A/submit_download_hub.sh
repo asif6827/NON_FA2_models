8bca777dc044b57cdbfb7940ccec4cc0e8149b68
asif6827 <asif6827@gmail.com>
2026-04-19 11:57:47 +0500

    Update submit_download_hub.sh

--- Reasoning360_sys_A/submit_download_hub.sh
+++ Reasoning360_sys_A/submit_download_hub.sh
@@ -6,6 +6,8 @@
 #sbatch ./Prompts_System/run_prompts_A100.sh
 
 
-echo "Downloading DeepSeek LLM"
-sbatch ./scripts_data_etc/download_hub_Deep_seek.sh
+#echo "Downloading DeepSeek LLM"
+#sbatch ./scripts_data_etc/download_hub_Deep_seek.sh
 
+echo "Downloading Qwen 2.5 3B"
+sbatch ./scripts_data_etc/download_hub_Qwen2.sh
