1f87f2d31c85d9257597f17aee20373eb8684bb2
asif6827 <asif6827@gmail.com>
2026-04-22 21:20:36 +0500

    update

---
+++ Reasoning360_sys_A_AR_LSAT/scripts_data_etc/download_hub_LLama3.py
@@ -0,0 +1,19 @@
+from huggingface_hub import snapshot_download
+from pathlib import Path
+
+# Llama 3.2 3B Instruct model
+repo_id = "meta-llama/Llama-3.2-3B-Instruct"
+
+# Change path as needed
+target = Path("/export/home/asifali/HF_cache/Llama-3.2-3B-Instruct")
+
+path = snapshot_download(
+    repo_id=repo_id,
+    repo_type="model",
+    local_dir=target,
+    local_dir_use_symlinks=False,   # store real files
+    resume_download=True,           # resume if interrupted
+    local_files_only=False          # force online download
+)
+
+print("Downloaded to:", path)
---
+++ Reasoning360_sys_A_AR_LSAT/scripts_data_etc/download_hub_Qwen2.py
@@ -0,0 +1,22 @@
+from huggingface_hub import snapshot_download
+from pathlib import Path
+
+hp = False
+panther = True
+if hp:
+    repo_id = "Qwen/Qwen2.5-3B-Instruct"           # or "Qwen/Qwen3-1.7B-Instruct"
+    target   = Path("/home/asif/data3/HF_Cache/Qwen2.5-3B-Instruct")  # your folder
+elif panther:
+    repo_id = "Qwen/Qwen2.5-3B-Instruct"
+    target = Path("/export/home/asifali/HF_cache/Qwen2.5-3B-Instruct")  # your folder
+
+path = snapshot_download(
+    repo_id=repo_id,
+    repo_type="model",
+    local_dir=target,
+    local_dir_use_symlinks=False,   # real files (not symlinks), avoids mount issues
+    resume_download=True,           # good for flaky networks
+    local_files_only=False          # ensure it goes online
+)
+print("Downloaded to:", path)
+
