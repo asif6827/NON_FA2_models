a49d11c83bdb35fe020ca09ebc74077bbc72f682
asif6827 <asif6827@gmail.com>
2026-04-27 03:39:15 +0500

    Update submit_job_data_process.sh

--- Reasoning360_sys_B1_AR_LSAT_Assignment/submit_job_data_process.sh
+++ Reasoning360_sys_B1_AR_LSAT_Assignment/submit_job_data_process.sh
@@ -3,7 +3,7 @@
 #echo " I am only this..!"
 
 echo "Data Processing script Running..!"
-sbatch ./data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
+sbatch ./data_preprocess/logic/v6a/data_preprocess_AR_LSAT_v6a.sh
 
 
 #echo "Data Pre-Processing Runing"
