e3cf1defe201f2db26e42ebdd8cf9b26eb4e6d4c
asif6827 <asif6827@gmail.com>
2026-04-27 22:43:09 +0500

    Update MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh

--- Reasoning360_sys_B_v29_AR_LSAT_Ordering/scripts_Qwen3_4B/train/Parsed_v2/MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B_v29_AR_LSAT_Ordering/scripts_Qwen3_4B/train/Parsed_v2/MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
@@ -2,7 +2,6 @@
 
 #SBATCH -J MLXL-B-v29-H200-Qwen3-4B-ARLSAT-Ordering #job name
 #SBATCH -p gpu-H200 # queue used
-#SBATCH --exclude=crirdchpxd001,crirdchpxd003
 #SBATCH --gres gpu:4 #number of gpus needed, default is 1
 #SBATCH -c 128  #number of CPUs needed, default is 1
 #SBATCH --mem 256GB #amount of memory needed, default
