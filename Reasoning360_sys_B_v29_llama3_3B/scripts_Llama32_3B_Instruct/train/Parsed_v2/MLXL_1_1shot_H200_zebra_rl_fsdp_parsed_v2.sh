cd748f3843b57b27742fe9153c852549db57d8c5
asif6827 <asif6827@gmail.com>
2026-05-22 18:53:07 +0500

    Update MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh

--- Reasoning360_sys_B_v29_llama3_3B/scripts_Llama32_3B_Instruct/train/Parsed_v2/MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B_v29_llama3_3B/scripts_Llama32_3B_Instruct/train/Parsed_v2/MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
@@ -204,8 +204,8 @@ adv_estimator=grpo
 
 use_kl_in_reward=False
 kl_coef=0.00
-use_kl_loss=True
-kl_loss_coef=0.0005
+use_kl_loss=False
+kl_loss_coef=0.00
 
 
 clip_ratio_low=0.2
@@ -284,7 +284,7 @@ python -m recipe.dapo.main_dapo \
     actor_rollout_ref.actor.use_dynamic_bsz=${use_dynamic_bsz} \
     actor_rollout_ref.actor.ppo_max_token_len_per_gpu=${actor_ppo_max_token_len} \
     actor_rollout_ref.actor.strategy="fsdp" \
-    actor_rollout_ref.actor.optim.lr=5e-7 \
+    actor_rollout_ref.actor.optim.lr=1e-5 \
     actor_rollout_ref.actor.optim.lr_warmup_steps=50 \
     actor_rollout_ref.actor.optim.weight_decay=0.1 \
     actor_rollout_ref.actor.optim.warmup_style=constant \
