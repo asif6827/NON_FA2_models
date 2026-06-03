bf1c074239707045325c694c4b624e5b88921dcd
asif6827 <asif6827@gmail.com>
2026-06-03 21:46:28 +0500

    Update our_puzzle_dataset_v6.py

--- Reasoning360_sys_B_v29_Qwen3_8B/verl/utils/reward_score/our_puzzle_dataset_v6.py
+++ Reasoning360_sys_B_v29_Qwen3_8B/verl/utils/reward_score/our_puzzle_dataset_v6.py
@@ -1009,7 +1009,8 @@ def compute_score(
                 reward = (
                         0.15 * parsing_reward
                         + 0.10 * format_reward
-                        + 0.60 * float(puzzle_acc_score))
+                        + 0.60 * float(puzzle_acc_score)
+                )
             else:
                 base_quality = (
                         0.60 * float(puzzle_acc_score)
