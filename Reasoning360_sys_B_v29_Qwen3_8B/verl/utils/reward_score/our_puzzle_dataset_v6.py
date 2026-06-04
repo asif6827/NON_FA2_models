f986bc3acb3b2770554bd9ff1b725b9f8c354251
asif6827 <asif6827@gmail.com>
2026-06-04 14:30:51 +0500

    Update our_puzzle_dataset_v6.py

--- Reasoning360_sys_B_v29_Qwen3_8B/verl/utils/reward_score/our_puzzle_dataset_v6.py
+++ Reasoning360_sys_B_v29_Qwen3_8B/verl/utils/reward_score/our_puzzle_dataset_v6.py
@@ -1021,7 +1021,7 @@ def compute_score(
                 process_bonus = (
                         0.40 * novel_step_score
                         + 0.30 * consistency_score
-                        - 0.30 * contradiction_ratio
+                        - 0.15 * contradiction_ratio
                 )
 
                 # gate process reward by solution quality
