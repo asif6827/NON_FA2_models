d418216e887b4f5aab58e209e49eef33d7b4dbbf
asif6827 <asif6827@gmail.com>
2026-06-18 20:04:40 +0500

    Update

--- Reasoning360_sys_B_v4/data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
+++ Reasoning360_sys_B_v4/data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
@@ -46,6 +46,8 @@ python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6
 
 python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_MLXL_EXAONE.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'mlxl_train_mlxl_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_MLXL'
 
+python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_MLXL_SmolLM3.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'mlxl_train_mlxl_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_MLXL'
+
 #python ./data_preprocess/logic/v6a/our_pre_process_zebrapuzzle_to_guru_parsed_v6a_ZTZT.py --data_path '/export/home/asifali/HF_cache/ZebraLogic' --data_setting 'zl_train_zl_test' --output_dir '/export/home/asifali/HF_cache/ZebraPuzzle_to_guru_parsed_v6a_ZTZT'
 
 #python ./data_preprocess/logic/our_pre_process_guru_readjust_parsed_v2.py --input_path '/export/home/asifali/HF_cache/guru_data' --output_dir '/export/home/asifali/HF_cache/guru_data_adjusted_parsed_v2'
