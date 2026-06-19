426771839c4957ffd2be0ee7712c055a2e8c7604
asif6827 <asif6827@gmail.com>
2026-06-19 17:06:57 +0500

    Update download_hub_Gemma.py

--- Reasoning360_sys_B_v4/scripts_data_etc/download_hub_Gemma.py
+++ Reasoning360_sys_B_v4/scripts_data_etc/download_hub_Gemma.py
@@ -1,6 +1,37 @@
 from huggingface_hub import snapshot_download
 from pathlib import Path
 
+###################################################################################
+
+repo_id = "google/gemma-2-2b-it"
+target = Path("/export/home/asifali/HF_cache/gemma-2-2b-it")  # your folder
+
+path = snapshot_download(
+    repo_id=repo_id,
+    repo_type="model",
+    local_dir=target,
+    local_dir_use_symlinks=False,   # real files (not symlinks), avoids mount issues
+    resume_download=True,           # good for flaky networks
+    local_files_only=False          # ensure it goes online
+)
+print("Downloaded to:", path)
+
+
+###################################################################################
+
+repo_id = "google/gemma-3-1b-it"
+target = Path("/export/home/asifali/HF_cache/gemma-3-1b-it")  # your folder
+
+path = snapshot_download(
+    repo_id=repo_id,
+    repo_type="model",
+    local_dir=target,
+    local_dir_use_symlinks=False,   # real files (not symlinks), avoids mount issues
+    resume_download=True,           # good for flaky networks
+    local_files_only=False          # ensure it goes online
+)
+print("Downloaded to:", path)
+
 
 ###################################################################################
 
@@ -16,7 +47,3 @@ path = snapshot_download(
     local_files_only=False          # ensure it goes online
 )
 print("Downloaded to:", path)
-
-
-###################################################################################
-
