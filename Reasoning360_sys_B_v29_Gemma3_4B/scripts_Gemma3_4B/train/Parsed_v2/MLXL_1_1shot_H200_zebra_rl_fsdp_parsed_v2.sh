8bbb190be373c3178d709cfae1de1aeedf56bcf4
asif6827 <asif6827@gmail.com>
2026-06-23 14:20:10 +0500

    Update MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh

--- Reasoning360_sys_B_v29_Gemma3_4B/scripts_Gemma3_4B/train/Parsed_v2/MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B_v29_Gemma3_4B/scripts_Gemma3_4B/train/Parsed_v2/MLXL_1_1shot_H200_zebra_rl_fsdp_parsed_v2.sh
@@ -351,7 +351,7 @@ python -m recipe.dapo.main_dapo \
     trainer.n_gpus_per_node=${NUM_GPUS} \
     trainer.nnodes=1 \
     trainer.test_freq=${TEST_FREQUENCY} \
-    trainer.save_freq=1 \
+    trainer.save_freq=1000 \
     trainer.max_actor_ckpt_to_keep=1 \
     trainer.max_critic_ckpt_to_keep=1 \
     trainer.total_epochs=${EPOCH} \
