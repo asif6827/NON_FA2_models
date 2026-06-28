56999613879021e16c41d4597535208a684101fc
asif6827 <asif6827@gmail.com>
2026-06-28 17:00:29 +0500

    Update

---
+++ Reasoning360_sys_B_v4_data_models/submit_job_data_process.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#echo " I am only this..!"
+
+
+mkdir -p ./all_logs
+
+echo "Data Processing script Running..!"
+sbatch ./data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
+
+
+#echo "Data Pre-Processing Runing"
+#sbatch ./data_preprocess/logic/v6a/data_preprocess_parsed_v6a_ALL.sh
