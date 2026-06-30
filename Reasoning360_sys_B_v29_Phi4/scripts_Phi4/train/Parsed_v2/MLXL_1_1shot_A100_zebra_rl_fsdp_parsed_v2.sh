3f7ec7924abfc204861114bff229f19e32d0e4bd
asif6827 <asif6827@gmail.com>
2026-07-01 02:02:04 +0500

    Update MLXL_1_1shot_A100_zebra_rl_fsdp_parsed_v2.sh

--- Reasoning360_sys_B_v29_Phi4/scripts_Phi4/train/Parsed_v2/MLXL_1_1shot_A100_zebra_rl_fsdp_parsed_v2.sh
+++ Reasoning360_sys_B_v29_Phi4/scripts_Phi4/train/Parsed_v2/MLXL_1_1shot_A100_zebra_rl_fsdp_parsed_v2.sh
@@ -86,7 +86,7 @@ echo "Python Path = ${PYTHONPATH}"
 # =================== User-Configurable Settings ===================
 # --- Execution Environment ---
 NUM_GPUS=4 # Set the number of GPUs to use on this node
-gpu_memory_utilization=0.7
+gpu_memory_utilization=0.5
 # --- Resuming & Logging ---
 RESUME_CKPT_DIR_NAME=""  # Fill in the W&B experiment name to resume from, otherwise leave empty to start from scratch
 WANDB_PROJECT="Sys_B_v1_Qwen3_4B_MLXL_1_1shot_H200" # Your wandb project name
