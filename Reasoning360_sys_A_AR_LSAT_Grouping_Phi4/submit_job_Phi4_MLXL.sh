e23d83ef431a081f84db790dd827cdc3136fc358
asif6827 <asif6827@gmail.com>
2026-07-06 17:03:57 +0500

    update

--- Reasoning360_sys_A_AR_LSAT_Grouping_Phi4/submit_job_Phi4_MLXL.sh
+++ Reasoning360_sys_A_AR_LSAT_Grouping_Phi4/submit_job_Phi4_MLXL.sh
@@ -15,7 +15,7 @@ TEST_LIST=(3)
 ACC_W_LIST=(0.8)
 Z3_W_LIST=(0.2)
 SWITCH_EPOCH_LIST=(80)
-SYSTEM_NAME_LIST=("Reasoning360_sys_A_AR_LSAT_Grouping")
+SYSTEM_NAME_LIST=("Reasoning360_sys_A_AR_LSAT_Grouping_Phi4")
 EVAL_PATH_LIST=("mlxl_train_mlxl_test_1_parsed_v6a_${ACRONYM}")
 DATA_PATH_LIST=("AR_LSAT_grouping_A_v6a_${ACRONYM}/mlxl_train_mlxl_test")
 
