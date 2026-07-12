4828d8caec6e02829c21306176ce9a209350cdb4
asif6827 <asif6827@gmail.com>
2026-07-12 16:02:43 +0500

    update

--- Reasoning360_sys_B_v29_llama3_v2_3B_N/submit_job_llama3_3B_MLXL_A100.sh
+++ Reasoning360_sys_B_v29_llama3_v2_3B_N/submit_job_llama3_3B_MLXL_A100.sh
@@ -16,7 +16,7 @@ TEST_LIST=(3)
 ACC_W_LIST=(0.9)
 Z3_W_LIST=(0.2)
 SWITCH_EPOCH_LIST=(80)
-SYSTEM_NAME_LIST=("Reasoning360_sys_B_v29_llama3_v2_3B")
+SYSTEM_NAME_LIST=("Reasoning360_sys_B_v29_llama3_v2_3B_N")
 EVAL_PATH_LIST=("mlxl_train_mlxl_test_1_parsed_v6a_${ACRONYM}")
 DATA_PATH_LIST=("ZebraPuzzle_to_guru_parsed_v6a_${ACRONYM}/mlxl_train_mlxl_test")
 
