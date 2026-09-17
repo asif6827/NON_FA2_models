5a9b16d190807ae4a49a329627776a103546f8aa
asif6827 <asif6827@gmail.com>
2026-09-18 01:10:41 +0500

    update

--- Reasoning360_sys_B_v29_v5/submit_job_qwen3_4B_MLXL.sh
+++ Reasoning360_sys_B_v29_v5/submit_job_qwen3_4B_MLXL.sh
@@ -16,7 +16,7 @@ TEST_LIST=(3)
 ACC_W_LIST=(0.8)
 Z3_W_LIST=(0.2)
 SWITCH_EPOCH_LIST=(80)
-SYSTEM_NAME_LIST=("Reasoning360_sys_B_v29_v4")
+SYSTEM_NAME_LIST=("Reasoning360_sys_B_v29_v5")
 EVAL_PATH_LIST=("mlxl_nss_mlxl_nss_1_parsed_v7_${ACRONYM}")
 DATA_PATH_LIST=("ZebraPuzzle_to_guru_parsed_v7_${ACRONYM}/mlxl_nss_mlxl_nss")
 
